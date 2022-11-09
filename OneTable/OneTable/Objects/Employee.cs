312ab1deef2f6f81d623cb631d2afc54b077fe39
nesterenkosu <62102496+nesterenkosu@users.noreply.github.com>
2022-11-09 13:32:56 +0500

    Add files via upload

---
+++ OneTable/OneTable/Objects/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTable
+{
+    class Employee
+    {
+        public virtual Guid ID { get; set; }
+        public virtual string Imya { get; set; }
+        public virtual int Age { get; set; }
+    }
+}
---
+++ OneTable/OneTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace OneTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Configuration();
+            c.Configure();
+            c.AddAssembly("OneTable");
+            new SchemaExport(c).Execute(true, true, false);
+
+            ISession session = c.BuildSessionFactory().OpenSession();
+
+            var vasya = new Employee();
+            vasya.Imya = "Sami ponimaete vasya";
+            vasya.Age = 40;
+            session.Save(vasya);
+            session.Flush();
+
+            var kolya = new Employee();
+            kolya.Imya = "A eto Kolya";
+            kolya.Age = 10;
+            session.Save(kolya);
+            session.Flush();
+
+            IList<Employee> list = session.QueryOver<Employee>().Where(x => x.Age > 5).List<Employee>();
+            foreach (Employee emp in list)
+                Console.WriteLine(emp.ID + " " + emp.Imya);
+
+
+            Console.ReadLine();
+        }
+    }
+}
