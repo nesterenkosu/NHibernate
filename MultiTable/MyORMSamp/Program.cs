7eaaf7036e17d496fccb4425b59aa0c9676cb7f4
nesterenkosu <62102496+nesterenkosu@users.noreply.github.com>
2022-11-09 13:30:26 +0500

    Add files via upload

---
+++ MultiTable/MyORMSamp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace MyORMSamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Инициализация NHibernate
+            InitHibernate();  
+
+            Console.ReadLine();
+        }
+
+        static void InitHibernate()
+        {
+            //Создание таблицы БД на основе
+            //файлов ORM-отображения (маппинга)
+            var cfg = new Configuration();
+            //Считывание и обработка файла hibernate.cfg.xml
+            cfg.Configure();
+            //Привязка к сборке, содержащей объекты ORM
+            cfg.AddAssembly("MyORMSamp");
+            //Собственно создание таблиц
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+    }
+}
